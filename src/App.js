9f7627849e0f05f6e3d7a10a8c35f7e63d916012
sayan49 <64882726+sayan49@users.noreply.github.com>
2021-10-31 16:11:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState();
+  const getUsers = async () => {
+    const response = await fetch("https://reqres.in/api/users?");
+    const data = await response.json();
+    setUsers(data.data);
+  };
+
+  return (
+      <>
+      <header className="header">
+        <h1>LetsGrowMore</h1>
+        <div className="buton" onClick={getUsers}>
+          GET USERS</div>
+      </header>
+      <div className="row">
+        {users?.map((current, index) => {
+          return (
+            <div className="card column" key={index}>
+              <img src={current.avatar} className="card-image" alt="..." />
+              <div className="card-body">
+                <h3 className="card-name">
+                  {current.first_name} {current.last_name}
+                </h3>
+                <h4 className="card-txt">{current.email}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+export default App;
